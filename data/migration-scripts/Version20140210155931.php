fcb0f008d8b885be22c1c68bc963c1e95bce2253
Sujai SD <sujaisd@gmail.com>
2014-03-30 22:57:50 +0530

    Added support for ZFCUser

--- data/migration-scripts/Version20140210155931.php
+++ data/migration-scripts/Version20140210155931.php
@@ -13,7 +13,7 @@ class Version20140210155931 extends AbstractMigration
     public function up(Schema $schema)
     {
         //Add user table for security
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, state INT NOT NULL, gender INT NOT NULL, phone_number INT NOT NULL, address INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;');
+        $this->addSql('CREATE TABLE `user` (`user_id` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,`username` VARCHAR(255) DEFAULT NULL UNIQUE, `email` VARCHAR(255) DEFAULT NULL UNIQUE, `display_name` VARCHAR(50) DEFAULT NULL, `password` VARCHAR(128) NOT NULL, `state` SMALLINT ) ENGINE=InnoDB;');
         
         //Add album table for functionality
         $this->addSql('CREATE TABLE album (id INT AUTO_INCREMENT NOT NULL, artist VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;');        
